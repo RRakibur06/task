82bc979c55ec943004621b7036b5ac152b9f5b56
Rakibur Rahman <rakib.230903@icrew-tech.com>
2024-02-11 00:22:32 +0600

    commit

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request 
+import os
 
 app = Flask(__name__, template_folder='templates') 
+upload_folder = os.path.join('static', 'Image')
+ 
+app.config['UPLOAD'] = upload_folder
 
 @app.route('/') 
 def index():
@@ -9,7 +13,9 @@ def index():
 
 @app.route('/resume', methods=['POST']) 
 def calculate():
-    image = request.form['image']
+    file = request.files['image']
+    file.save(os.path.join(app.config['UPLOAD'], file.filename))
+    img = os.path.join(app.config['UPLOAD'], file.filename)
     name = request.form['name'] 
     post = request.form['post'] 
     email = request.form['email']
@@ -30,7 +36,7 @@ def calculate():
     project_details_two = request.form['project_details_two']
     project_name_three = request.form['project_name_three']
     project_details_three = request.form['project_details_three']
-    return render_template('resume.html',image=image,name=name,post=post,email=email,address=address,skills=skills,certification_one=certification_one,certification_two=certification_two,
+    return render_template('resume.html',image=img,name=name,post=post,email=email,address=address,skills=skills,certification_one=certification_one,certification_two=certification_two,
      organization_name=organization_name,position=position,duration=duration,experience=experience,
      edu_organization_name=edu_organization_name,degree=degree,edu_duration=edu_duration,
      project_name_one=project_name_one,project_details_one=project_details_one,                    
